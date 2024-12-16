3a3d850030b4796feb01e8102e9facbbb9b4926f
spcodegen <shehansp2000@gmail.com>
2024-12-16 12:59:14 +0530

    chatBox project complete

--- app.js
+++ app.js
@@ -1,13 +1,61 @@
-let isClicked;
+var md = window.markdownit();
 
-$('#btnAdd').click(function (e) {
-    isClicked =true;
-    if (isClicked) {
-        $('h1,p').addClass('back-color');
-    } else {
-        $('h1,p').removeClass('back-color');
-    } 
-    isClicked=false;   
+function chatWithSaman() {
+    let userInput = document.getElementById('txtUserInput').value;
+    console.log(userInput);
+
+    document.getElementById("chatBox").innerHTML += `
+                    <li class="d-flex gap-4 justify-content-end">
+                        <div>
+                          <h6 class="mb-0">*Me</h6>
+                          ${userInput}
+                        </div>
+                    </li>`
+
+
+
+
+// ---------------------------------------------------------------------------------------
+
+
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+  "contents": [
+    {
+      "parts": [
+        {
+          "text": userInput
+        }
+      ]
+    }
+  ]
 });
 
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
 
+fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyDMhRBphpmQFltPuvaEhgZQRfOd49AHRgI", requestOptions)
+  .then((response) => response.json())
+  .then((result) => {
+    console.log()
+
+
+    document.getElementById("chatBox").innerHTML += `
+                    <li class="d-flex gap-4">
+                        <div>
+                          <h6 class="mb-0">*Saman</h6>
+                          ${md.render(result.candidates[0].content.parts[0].text)}
+                        </div>
+                    </li>`
+  })
+  .catch((error) => console.error(error));
+
+
+    
+}
